f6c546436031206b599bf153ffe96df244e827b4
clarapic5 <clara.picazo@est.fib.upc.edu>
2019-12-01 00:31:19 +0100

    profile

--- src/components/Game/components/PlayerProfile/PlayerProfile.jsx
+++ src/components/Game/components/PlayerProfile/PlayerProfile.jsx
@@ -26,7 +26,7 @@ class PlayerProfile extends Component {
           <span>Lost <span className="count">{ lostCount }</span></span>
         </div>
         <div className="buttons">
-          <Button onClick={ onStartGame } className="green">START</Button>
+          <Button onClick={ onStartGame } className="green">IMPORT</Button>
         </div>
       </div>
     )
