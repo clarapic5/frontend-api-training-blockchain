261a28ab012e1916676df1c9c010d45029d913dc
clarapic5 <clara.picazo@est.fib.upc.edu>
2019-12-11 16:59:27 +0100

    button upload file WORKING!!

--- src/components/UploadActivity/UploadActivity.jsx
+++ src/components/UploadActivity/UploadActivity.jsx
@@ -1,43 +1,71 @@
 import React, { Component } from 'react';
-import { Button } from 'components';
 import { ApiService } from 'services';
+import axios, { post } from 'axios';
+import { Button } from 'components';
 
 class UploadActivity extends Component {
 
   constructor(props) {
-
     super(props);
     this.state = {
+      file: null,
       activities: []
-    };
- 
-    this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    this.onFormSubmit = this.onFormSubmit.bind(this)
+    this.onChange = this.onChange.bind(this)
+    this.fileUpload = this.fileUpload.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
-
-
+  onFormSubmit(e) {
+    e.preventDefault() // Stop form submit
+    this.fileUpload(this.state.file).then((response) => {
+      console.log(response.data);
+    })
+  }
+  onChange(e) {
+    this.setState({ file: e.target.files[0] })
+  }
+  fileUpload(file) {
+    const url = 'http://example.com/file-upload';
+    const formData = new FormData();
+    formData.append('file', file)
+    const config = {
+      headers: {
+        'content-type': 'multipart/form-data'
+      }
+    }
+    return post(url, formData, config)
+  }
 
   handleSubmit(event) {
     event.preventDefault();
-    
-    return ApiService.insert(2,1,1,1,1,1,1,1,1,1)
+
+    return ApiService.insert(31, 1, 1, 1, 1, 1, 1, 1, 1, 1)
       .catch(err => {
         localStorage.setItem("error", err);
       });
   }
 
+
   render() {
     return (
       <div className="UploadActivity">
         <div className="title">Sports Activity Manager EOS</div>
-        <div className="description">Please use the Account Name and Private Key generated in the beginning to log into the system.</div>
+        <div className="description">Please select the file .FIT to import</div>
+        <form name="form" onSubmit={this.onFormSubmit}>
+          <input type="file" onChange={this.onChange} />
+          <button type="submit">Upload</button>
           <div className="bottom">
             <Button onClick={this.handleSubmit.bind(this)} type="submit" className="green">
               {"PUT"}
             </Button>
           </div>
-      
-      </div>
-    )
-  }
-}
-export default UploadActivity;
+        </form>
+        </div>
+        )
+      }
+    }
+    
+    
+    
+export default UploadActivity
