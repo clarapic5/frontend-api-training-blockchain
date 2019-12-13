beecacfceff73cfd2184e7af8e9fd5787783647f
clarapic5 <clara.picazo@est.fib.upc.edu>
2019-12-13 10:17:47 +0100

    upload activity

--- src/components/UploadActivity/UploadActivity.jsx
+++ src/components/UploadActivity/UploadActivity.jsx
@@ -8,15 +8,39 @@ class UploadActivity extends Component {
   constructor() {
     super();
     this.state = {
-      userList: []
+      selectedFile: null,
+      activities: []
     };
    }
+
+
+   //GET FILE! OK
+  onChangeHandler = event => {
+    this.setState({
+      selectedFile: event.target.files[0],
+      loaded: 0,
+    })
+  }
+
+  //COMO GUARDAR??
+  onClickHandler = () => {
+    const data = new FormData()
+    data.append('file', this.state.selectedFile)
+    console.log(this.state.selectedFile)
+   // axios.post("http://localhost:8000/upload", data, {
+   // })
+    //  .then(res => { 
+      //  console.log(res.statusText)
+     // })
+  }
     
+
+  //OBTIENE ARRAY DE LA URL! OK
    componentWillMount() {
     axios.get('https://api.myjson.com/bins/asc4k') // JSON File Path
       .then( response => {
         this.setState({
-        userList: response.data
+        activities: response.data
       });
     })
     .catch(function (error) {
@@ -25,19 +49,19 @@ class UploadActivity extends Component {
    }
 
   render() {
-    const usersList = this.state.userList;
-    console.log(usersList);
+    const activities = this.state.activities;
+    console.log(activities);
     console.log("mee he ejecutadoo");
     return (
       <div className="UploadActivity">
         <div className="title">Sports Activity Manager EOS</div>
         <div className="description">Please select the file .FIT to import</div>
         <form name="form" >
-          <input type="file" />
-          <button type="submit">Upload</button>
+        <input type="file" name="file" onChange={this.onChangeHandler} />
+        <button type="button" onClick={this.onClickHandler}>Upload</button>
           <div className="bottom">
             <Button  type="submit" className="green">
-              {"PUT"}
+              {"Back"}
             </Button>
           </div>
         </form>
