65f7bbc429f05c6869764276ee966e162c960869
clarapic5 <clara.picazo@est.fib.upc.edu>
2019-12-14 16:39:08 +0100

    backup GET JSON FILE WORKS!!

--- server.js
+++ server.js
@@ -29,6 +29,25 @@ app.post('/upload', upload.single('image'), (req, res) => {
         res.status("409").json("No Files to Upload.")
 });
 
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+/*
+// GET method route
+app.get('/', function(req, res) {
+    res.send('GET request to the homepage');
+});
+
+// POST method route
+app.post('/', function(req, res) {
+    res.send('POST request to the homepage');
+});
+
+*/
+
+
 const PORT = 5000;
 
 app.listen(PORT);
--- src/components/UploadActivity/UploadActivity.jsx
+++ src/components/UploadActivity/UploadActivity.jsx
@@ -4,72 +4,98 @@ import axios from 'axios';
 const BASE_URL = 'http://localhost:5000/';
 
 class UploadActivity extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        	files: [],
-        	fileUrls: [],
-        	message: ''
-        }
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			files: [],
+			fileUrls: [],
+			extension: ' ',
+			activities: [],
+			message: ''
+		}
+		this.componentWillMount = this.componentWillMount.bind(this);
+	}
 
-    selectFiles = (event) => {
-    	let files = [];
-    	for (var i = 0; i < event.target.files.length; i++) {
-            files[i] = event.target.files.item(i);
-        }
-	    files = files.filter(file=> file.name.match(/\.(json)$/))
-        let message = `${files.length} valid file(s) selected`
-        this.setState({ files, message })
-    }
+	selectFiles = (event) => {
+		let files = [];
+		for (var i = 0; i < event.target.files.length; i++) {
+			files[i] = event.target.files.item(i);
+		}
+		files = files.filter(file => file.name.match(/\.(json)$/))
+		let message = `${files.length} valid file(s) selected`
+		this.setState({ files, message })
+	}
 
-    uploadFiles = () => {
-    	const uploaders = this.state.files.map(file => {
-		    const data = new FormData();
-		    data.append("image", file, file.name);
-		    
-	    	// Make an AJAX upload request using Axios
-	    	return axios.post(BASE_URL + 'upload', data)
-	    	.then(response => {
-				this.setState({fileUrl: [response.data.fileUrls, ...this.state.fileUrls]});
+	uploadFiles = () => {
+		const uploaders = this.state.files.map(file => {
+			const data = new FormData();
+			data.append("image", file, file.name);
+			this.setState({
+				extension: file.lastModified + '-' + file.name
 			})
+			// Make an AJAX upload request using Axios
+			axios.post(BASE_URL + 'upload', data)
+				.then(response => {
+					this.setState({ fileUrl: [response.data.fileUrls, ...this.state.fileUrls] });
+				})
 		});
 
-	 	// Once all the files are uploaded 
+		// Once all the files are uploaded 
 		axios.all(uploaders).then(() => {
 			console.log('done');
 		}).catch(err => alert(err.message));
+	}
 
-    }
+	componentWillMount() {
+		/*	const ext = this.state.extension;
+			console.log(ext);
+			console.log(BASE_URL + 'uploads/' + ext);
+			let url = 'https://api.myjson.com/bins/asc4k';
+			axios.get(url) // JSON File Path
+				.then(response => {
+					this.setState({
+						activities: response.data
+					});
+				})
+				.catch(function (error) {
+					console.log(error);
+				});*/
+		
+		// Once all the files are uploaded 
+	}
 
-    render() {
-        return (
-        	<div>
-	        	<br/>
-	        	<div className="col-sm-12">
-        			<h1>Image Uploader</h1><hr/>
-	        		<div className="col-sm-4">
-		        		<input className="form-control " type="file" onChange={this.selectFiles} multiple/>
-		        	</div>
-		        	{ this.state.message? <p className="text-info">{this.state.message}</p>: ''}
-		        	<br/><br/><br/>
-		        	<div className="col-sm-4">
-		            	<button className="btn btn-primary" value="Submit" onClick={this.uploadFiles}>Submit</button>
-		        	</div>
-	            </div>
-	            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><hr/><br/>
-	            <div className="row col-lg-12">
-		        	{ 
-			          	this.state.fileUrls.map((url, i) => (
-				          		<div className="col-lg-2" key={i}>
-				          			<img src={BASE_URL + url} className="img-rounded img-responsive" alt="not available"/><br/>
-				          		</div>
-				          	))
-			        }
-		        </div>
-		    </div>
-        );
-    }
+	render() {
+		return (
+			<div>
+				<br />
+				<div className="col-sm-12">
+					<h1>Image Uploader</h1><hr />
+					<div className="col-sm-4">
+						<input className="form-control " type="file" onChange={this.selectFiles} multiple />
+					</div>
+					{this.state.message ? <p className="text-info">{this.state.message}</p> : ''}
+					<br /><br /><br />
+					<div className="col-sm-4">
+						<button className="btn btn-primary" value="Submit" onClick={this.uploadFiles}>Submit</button>
+					</div>
+					<br /><br /><br />
+					<div className="col-sm-4">
+						<button className="btn btn-primary" value="Submit" onClick={this.componentWillMount}>Mount</button>
+					</div>
+				</div>
+				<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><hr /><br />
+				<div className="row col-lg-12">
+					{
+						this.state.fileUrls.map((url, i) => (
+							<div className="col-lg-2" key={i}>
+								<img src={BASE_URL + url} className="img-rounded img-responsive" alt="not available" /><br />
+							</div>
+						))
+					}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default UploadActivity; 			
