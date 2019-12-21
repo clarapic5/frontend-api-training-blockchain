002d7e9df329f5878f31b9eb598ad74badbdcfc7
clarapic5 <clara.picazo@est.fib.upc.edu>
2019-12-21 17:11:41 +0100

    COMPLETED!! activity graphs

--- src/components/ActivityView/ActivityView.jsx
+++ src/components/ActivityView/ActivityView.jsx
@@ -4,6 +4,11 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, } from '
 import heartRate from './images/heart-rate.png';
 import speed from './images/symbol-run.png';
 import time from './images/symbol-time.png'
+import distance from './images/symbol-distance.png'
+import altitude from './images/symbol-altitude.png'
+import calories from './images/symbol-calories.png'
+import temperature from './images/symbol-temperature.png'
+import weather from './images/symbol-weather.png'
 
 const heartRateData = [
   {
@@ -120,32 +125,30 @@ class ActivityView extends Component {
             </div>
 
             <div class="little">
-              <center> <img src={time} alt="symbol time" /></center>
-              <div class="centerRight">
+              <center> <img src={time} alt="symbol time" /></center>      
                 <h5>00:15:00 min </h5>
-              </div>
             </div>
             <div class="little">
-              <h2>3rd Content Area</h2>
-              <p>Illo quod nemo ratione itaque dolores laudantium error vero laborum blanditiis nostrum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ratione architecto cum praesentium voluptatibus recusandae?</p>
+            <center> <img src={distance} alt="symbol distance" /></center>      
+                <h5>120 km</h5>
             </div>
             <div class="little">
-              <h2>3rd Content Area</h2>
-              <p>Illo quod nemo ratione itaque dolores laudantium error vero laborum blanditiis nostrum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ratione architecto cum praesentium voluptatibus recusandae?</p>
+            <center> <img src={altitude} alt="symbol altitude" /></center>      
+                <h5>10 m </h5>
             </div>
           </section>
           <section class="columns">
             <div class="little">
-              <h2>3rd Content Area</h2>
-              <p>Illo quod nemo ratione itaque dolores laudantium error vero laborum blanditiis nostrum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ratione architecto cum praesentium voluptatibus recusandae?</p>
+            <center> <img src={weather} alt="symbol weather" /></center>      
+                <h5>Cloudy</h5>
             </div>
             <div class="little">
-              <h2>3rd Content Area</h2>
-              <p>Illo quod nemo ratione itaque dolores laudantium error vero laborum blanditiis nostrum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ratione architecto cum praesentium voluptatibus recusandae?</p>
+            <center> <img src={calories} alt="symbol calories" /></center>      
+                <h5>90 kcal </h5>
             </div>
             <div class="little">
-              <h2>3rd Content Area</h2>
-              <p>Illo quod nemo ratione itaque dolores laudantium error vero laborum blanditiis nostrum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ratione architecto cum praesentium voluptatibus recusandae?</p>
+            <center> <img src={temperature} alt="symbol temperature" /></center>      
+                <h5>13 ºC </h5>
             </div>
           </section>
           <br></br>
